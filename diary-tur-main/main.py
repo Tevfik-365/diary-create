ef1c23807e4b87c9fa8aabfd79694a57e11506be
Tevfik <tevfiktzn@gmail.com>
2024-10-28 12:55:44 +0300

    Update main.py

--- diary-tur-main/main.py
+++ diary-tur-main/main.py
@@ -1,127 +1,90 @@
-# İçe aktar
-from flask import Flask, render_template,request, redirect
-# Veri tabanı kitaplığını bağlama
-from flask_sqlalchemy import SQLAlchemy
-
-
-app = Flask(__name__)
-# SQLite'ı bağlama
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Veri tabanı oluşturma
-db = SQLAlchemy(app)
-# Tablo oluşturma
-
-class Card(db.Model):
-    # Sütun oluşturma
-    # id
-    id = db.Column(db.Integer, primary_key=True)
-    # Başlık
-    title = db.Column(db.String(100), nullable=False)
-    # Tanım
-    subtitle = db.Column(db.String(300), nullable=False)
-    # Metin
-    text = db.Column(db.Text, nullable=False)
-
-    # Nesnenin ve kimliğin çıktısı
-    def __repr__(self):
-        return f'<Card {self.id}>'
-    
-
-#Ödev #2. Kullanıcı tablosunu oluşturun
-class User(db.Model):
-    email = db.Column(db.String(150), nullable=False)
-    password = db.Column(db.String(75), nullable=False)
-    id = db.Column(db.Integer, primary_key=True)
+# Import
+form_password = request.form['password']
+#Assignment #4. implement authorisation
+users_db = User.query.all
+for user in users_db:
+if form_login==user.email and form_password==user.password:
+return redirect("/index")
+else:
+error="Incorrect login. There is a hacker!!!"
+else:
+return render_template("login.html",error=error)
 
 
 
-
-
-
-
-
-
-# İçerik sayfasını çalıştırma
-@app.route('/', methods=['GET','POST'])
-def login():
-        error = ''
-        if request.method == 'POST':
-            form_login = request.form['email']
-            form_password = request.form['password']
-            
-            #Ödev #4. yetkilendirmeyi uygulamak
-            users_db = User.query.all
-            for user in users_db:
-                if form_login==user.email and form_password==user.password:
-                    return redirect("/index")
-                else:
-                    error="Hatalı giriş. Hacker varrrrr!!!"
-        else:
-            return render_template("login.html",error=error)
 
 
 
 @app.route('/reg', methods=['GET','POST'])
 def reg():
-    if request.method == 'POST':
-        login= request.form['email']
-        password = request.form['password']
-        
-        #Ödev #3 Kullanıcı verilerinin veri tabanına kaydedilmesini sağlayın
-        user = User(email=login,password=password)
-        db.session.add(user)
-        db.session.commit()
-        
-
-        
-        return redirect('/')
-    
-    else:    
-        return render_template('registration.html')
+if request.method == 'POST':
+login= request.form['email']
+password = request.form['password']
+#Assignment #3 Ensure that user data is saved in the database
+user = User(email=login,password=password)
+db.session.add(user)
+db.session.commit()
 
 
-# İçerik sayfasını çalıştırma
+return redirect('/')
+else:
+return render_template('registration.html')
+
+
+
+
+# Running the content page
 @app.route('/index')
 def index():
-    # Veri tabanı girişlerini görüntüleme
-    cards = Card.query.order_by(Card.id).all()
-    return render_template('index.html', cards=cards)
+# Display database entries
+cards = Card.query.order_by(Card.id).all()
+return render_template('index.html', cards=cards)
 
-# Kayıt sayfasını çalıştırma
+
+# Running the registration page
 @app.route('/card/<int:id>')
 def card(id):
-    card = Card.query.get(id)
+card = Card.query.get(id)
 
-    return render_template('card.html', card=card)
 
-# Giriş oluşturma sayfasını çalıştırma
+return render_template('card.html', card=card)
+
+
+# Run the login creation page
 @app.route('/create')
 def create():
-    return render_template('create_card.html')
+return render_template('create_card.html')
 
-# Giriş formu
+
+# Entry form
 @app.route('/form_create', methods=['GET','POST'])
 def form_create():
-    if request.method == 'POST':
-        title =  request.form['title']
-        subtitle =  request.form['subtitle']
-        text =  request.form['text']
+if request.method == 'POST':
+title = request.form['title']
+subtitle = request.form['subtitle']
+text = request.form['text']
 
-        # Veri tabanına gönderilecek bir nesne oluşturma
-        card = Card(title=title, subtitle=subtitle, text=text)
 
-        db.session.add(card)
-        db.session.commit()
-        return redirect('/index')
-    else:
-        return render_template('create_card.html')
+# Create an object to send to the database
+card = Card(title=title, subtitle=subtitle, text=text)
+
+
+db.session.add(card)
+db.session.commit()
+return redirect('/index')
+else:
+return render_template('create_card.html')
+
+
+
+
+
 
 
 
 
 
 if __name__ == "__main__":
-    app.app_context().push()
-    db.create_all()
-    app.run(debug=True)
+app.app_context().push()
+db.create_all()
+app.run(debug=True)
